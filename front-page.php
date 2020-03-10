7dd1c0a66b3b6cece125574eccf5807152c3b248
Ross <lukaross06@gmail.com>
2020-03-10 17:20:33 -0400

    Création de la grille

--- front-page.php
+++ front-page.php
@@ -24,6 +24,7 @@ get_header();
                 <li><a href="#" style="text-decoration: none" target="_blank" rel="noopener noreferrer">Nouvelle</a></li>
             </ul>
             <h2>Les cours du programme de Techniques d'intégration multimédia du collège de Maiosnnueve</h2>
+            <div class ="petitGrid" style="display: grid; grid-template-columns:repeat(5,1fr); grid-template-rows:repeat(7, 1fr);">
 		<?php
 		while ( have_posts() ) :
 			the_post();
@@ -45,68 +46,101 @@ get_header();
 		);
         $query1 = new WP_Query($args);
         $i=1;
+        $arrayy = array_fill(0, 29, array_fill(0, 2, 0));
 		while ( $query1->have_posts() ) {   
             $query1->the_post();
+
+            
+
             
            
-           
-
-            // for($e =0; $e<29;$e++){
+        
                 $firstLetter = substr(get_the_title(),4,1);
                 $secondLetter= substr(get_the_title(),5,1);
-                $text1 = "";
-                $text2= "";
-                switch($firstLetter){
-                    case 1:
-                        $text1 = "session: 1";
-                        break;
-                    case 2:
-                        $text1 = "session: 2";
-                        break;
-                    case 3:
-                        $text1 = "session: 3";
-                        break;
-                    case 4:
-                        $text1 = "session: 4";
-                        break;
-                    case 5:
-                        $text1 = "session: 5";
-                        break;
-                    case 6:
-                        $text1 = "session: 6";
-                        break;
+                $r = 0;
+                $c = 0; 
+               
 
+    
+                    switch ($firstLetter) {
 
-                }
-                switch($secondLetter){
-                    case 1:
-                        $text2 = "domaine: 1";
-                        break;
-                    case 2:
-                        $text2 = "domaine: 2";
-                        break;
-                    case 3:
-                        $text2 = "domaine: 3";
-                        break;
-                    case 4:
-                        $text2 = "domaine: 4";
-                        break;
-                    case 5:
-                        $text2 = "domaine: 5";
-                        break;
-                    case 6:
-                        $text2 = "domaine: 6";
-                        break;
+                        case 1:
+                        $r=2;
+                            break;
+                        case 2:
+                        $r=3;
+                            break;
+                        case 3:
+                        $r=4;
+                            break;
+                        case 4:
+                            $r=5;
+                            break;
+                        case 5:
+                            $r=6;
+                            break;
+                        case 6:
+                            $r=7;
+                            break;
+                
+                        default: $r=0;
+                    }
+    
+                    switch ($secondLetter) {
+                        case 1:
+                        $c=1;
+                            break;
+                        case 2:
+                        $c=2;
+                            break;
+                        case 3:
+                        $c=3;
+                            break;
+                        case 4:
+                            $c=4;
+                            break;
+                        case 5:
+                            $c=5;
+                            break;
+                        case 6:
+                            $c=6;
+                            break;
+                
+                        default: $c=0;
+                    }
+                    $arrayy[$i][0] = $r;
+                    $arrayy[$i][1] = $c;
+                    // foreach ($arrayy as $current_key => $current_array) {
+                    //     $search_key = array_search($current_array, $arrayy);
+                    //     if ($current_key != $search_key) {
+                    //         $gridArea = $r ."/". $c ;
+                    //         echo '<h3 class="grid-cell dup" style="grid-area:'.$gridArea.'">'.'<p><a href="#">'.substr(get_the_title(),0,7).'</a><p>'.'</h3>';
+                    //         break;
+                    //     } else{
+                    //         $gridArea = $r ."/". $c ;
+                    //         echo '<h3 class="grid-cell" style="grid-area:'.$gridArea.'">'.'<p><a href="#">'.substr(get_the_title(),0,7).'</a><p>'.'</h3>';
+                    //         break;
+                    //     }
+                      
+                    // }
+                    
+             
+                     $gridArea = $r ."/". $c ;
 
+                     echo '<h3 class="grid-cell" style="grid-area:'.$gridArea.'">'.'<p><a href="#">'.substr(get_the_title(),0,7).'</a><p>'.'</h3>';
+                   
+                
+               
+                
 
-                }
-            // }
-           
-            echo '<h4 class="items">' .$i. '. <a href="#" class="title">' . get_the_title() . '</a>__' .'<span class="text1">'. $text1 .'</span>'. '__'.'<span class="text2">'. $text2 .'</span>'.'</h4>';
-            $i++;
-		}
+        
+
+             
+        }
+        
+       
 		?>
-
+    </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
